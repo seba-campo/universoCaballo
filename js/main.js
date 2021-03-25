ce0fcb94a570b646b2953a3e473d39cd7238f651
Sebastián <69972223+seba-campo@users.noreply.github.com>
2021-03-24 23:31:53 -0300

    Minor changes

--- js/main.js
+++ js/main.js
@@ -8,4 +8,31 @@ $(document).ready(function(){
         auto: true,
     });
         
+
+
+    
+    // Animaciones con jQuery de fadeIn
+    var img = $('#imagenBody');
+    var txt = $('#textoBody');
+
+    img.css('opacity', 0);
+    txt.css('opacity', 0);
+
+
+    fade_in(img);
+    $(window).scroll(function(){
+        fade_in(selector);
+    });
+
+    function fade_in(selector){
+        selector.each(function(){
+            let bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            let bottom_of_window = $(windown).scrollTop() + $(window).height();
+
+            if(bottom_of_window > bottom_of_element){
+                $(this).animate({'opacity':'1'}, 1500);
+            }
+
+        });
+    };
 });
