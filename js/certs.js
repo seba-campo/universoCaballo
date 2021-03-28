d37704cf1a7fa5a129e5f2a867f480e953320c2a
Sebastián <69972223+seba-campo@users.noreply.github.com>
2021-03-28 19:57:33 -0300

    Agregada seccion de certificados

--- js/certs.js
+++ js/certs.js
@@ -1,10 +1,21 @@
 $(document).ready(function(){
 
+    $('.bxslider').bxSlider({
+        captions: true,
+        randomStart: true,
+        mode: 'fade',
+        auto: true,
+        infiniteLoop: true,
+        slideWidth: 600,
+    });
+
+
+
     $(document).scroll(function () {
         var y = $(this).scrollTop();
         console.log(y);
         
-        if (y >= 100){
+        if (y >= 10){
             $('footer').fadeIn(1500);
         } 
     });
